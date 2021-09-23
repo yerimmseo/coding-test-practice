45b7368c3edffe26ee3715fb35631b5530263f59
yerimmseo <yerim7386@naver.com>
2021-09-23 15:08:17 +0900

    내적

---
+++ programmers/src/level1/InnerProduct.java
@@ -0,0 +1,11 @@
+package level1;
+
+public class InnerProduct {
+	public int solution(int[] a, int[] b) {
+		int answer = 0;
+		for (int i = 0; i < a.length; i++) {
+			answer += a[i] * b[i];
+		}
+		return answer;
+	}
+}
