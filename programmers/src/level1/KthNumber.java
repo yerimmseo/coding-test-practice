779fc91305226ee05f92d12f9acaaeef789f314e
yerimmseo <yerim7386@naver.com>
2021-09-23 23:57:10 +0900

    K번째 수

---
+++ programmers/src/level1/KthNumber.java
@@ -0,0 +1,45 @@
+package level1;
+
+public class KthNumber {
+	public int[] solution(int[] array, int[][] commands) {
+		int[] answer = new int[commands.length];
+		
+		for (int i = 0; i < commands.length; i++) {
+			int start = commands[i][0];
+			int end = commands[i][1];
+			int offset = commands[i][2]; 
+			
+			int[] arr = new int[end - start + 1];
+			int index = 0;
+			
+			for (int j = 0; j < array.length; j++) {
+				if (!(j >= start - 1 && j < end)) {
+					System.out.println(j);
+					continue;
+				}
+				arr[index] = array[j];
+				index++;
+			}
+			
+			for (int k = 0; k < arr.length; k++) {
+				for (int l = 0; l < arr.length - 1; l++) {
+					if (arr[l] > arr[l + 1]) {
+						int tmp = arr[l + 1];
+						arr[l + 1] = arr[l];
+						arr[l] = tmp;
+					}
+				}
+			}
+			answer[i] = arr[offset - 1];
+		}
+		
+		/* Arrays.copyOfRange : 배열을 복사하는 메서드 
+		for (int i = 0; i < commands.length; i++) {
+			int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+			Arrays.sort(temp);
+			answer[i] = temp[commands[i][2] - 1];
+		}
+		*/
+		return answer;
+	}
+} 
