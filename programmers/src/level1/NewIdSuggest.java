d8d134b97bba1e65523dcd306eb722506e3c4fec
yerimmseo <yerim7386@naver.com>
2021-09-23 10:14:01 +0900

    신규 아이디 추천

--- programmers/src/level1/NewIdSuggest.java
+++ programmers/src/level1/NewIdSuggest.java
@@ -3,26 +3,23 @@ package level1;
 public class NewIdSuggest {
 	public String solution(String new_id) {
 		String answer = "";
-		// 1단계
+
 		answer = new_id.toLowerCase();
-		// 2단계
 		answer = answer.replaceAll("[^0-9a-z_.-]+", "");
-		// 3단계
 		answer = answer.replaceAll("[.]{2,}", ".");
-		// 4단계
 		answer = answer.replaceAll("^[.]", "");
 		answer = answer.replaceAll("[.]$", "");
-		// 5단계
+
 		if (answer.isEmpty()) {
 			for (int i = 0; i < new_id.length(); i++) {
 				answer += "a";
 			}
 		}
-		// 6단계
+
 		if (answer.length() > 15) {
 			answer = answer.substring(0, 15).replaceAll("[.]$", "");
 		}
-		// 7단계
+
 		if (answer.length() <= 2) {
 			while (answer.length() != 3) {
 				answer += answer.substring(answer.length() - 1);
